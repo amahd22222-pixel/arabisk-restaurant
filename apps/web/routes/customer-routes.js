249bd196d38f8882ed696662e76f4c8ebfa102a5
amahd22222-pixel <amahd22222@gmail.com>
2026-09-25 20:06:23 +0400

    refactor: simplify customer routes

--- apps/web/routes/customer-routes.js
+++ apps/web/routes/customer-routes.js
@@ -1,14 +1,9 @@
-import { sendServiceError } from '../utils/service-error.js';
 export function registerCustomerRoutes(app, { service, requireAdminApiKey }) {
   app.get('/api/customers', requireAdminApiKey, (_req, res) => {
     return res.json(service.listCustomers());
   });
 
   app.patch('/api/customers/:id', requireAdminApiKey, (req, res) => {
-    try {
-      return res.json(service.updateCustomer(req.params.id, req.body || {}));
-    } catch (error) {
-      return sendServiceError(res, error);
-    }
+    return res.json(service.updateCustomer(req.params.id, req.body || {}));
   });
 }
