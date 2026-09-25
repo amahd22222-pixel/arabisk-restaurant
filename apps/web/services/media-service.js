033a1ac24d2f21fda5db76c6a2bc48c406e2ddd9
amahd22222-pixel <amahd22222@gmail.com>
2026-09-25 19:39:41 +0400

    refactor: use product repository in media service

--- apps/web/services/media-service.js
+++ apps/web/services/media-service.js
@@ -20,6 +20,7 @@ export function createMediaService({
   presign
 }) {
   const { products } = repository;
+  const { products } = repository;
   const productDetails = [];
   let detailsLoaded = false;
 
